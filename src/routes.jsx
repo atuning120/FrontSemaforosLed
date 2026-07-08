f640aafe1dca8f5b298fc36c04d7d0d39da21abd
atuning120 <cristian.ignacio.nunez@gmail.com>
2026-07-08 13:58:05 -0400

    rutas no existentes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import AdminApp from './components/admin/AdminApp.jsx';
+import NotFound from './components/NotFound.jsx';
 
 export default function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin/*" element={<AdminApp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
